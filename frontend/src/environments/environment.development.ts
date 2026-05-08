5e28f380cac96a3c992553106923328ca282ac49
latenight <maryavk2005@yandex.ru>
2026-05-08 13:36:12 +0500

    fix environments

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl: "localhost:8000",
+    apiUrl: "http://localhost:8000",
     prod: false
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+    apiUrl: "'http://localhost:8000",
+    prod: true
+};
